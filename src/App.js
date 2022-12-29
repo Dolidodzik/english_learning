04c9533fe1a4aaf73563564f0286485e81cfe416
Dolidodzik <teethtard321@gmail.com>
2022-12-29 15:59:29 +0100

    finished basic functionality

--- src/App.js
+++ src/App.js
@@ -1,40 +1,73 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Papa from 'papaparse'
 import Loading from './components/Loading'
+import Card from './components/Card'
+
+
 
 function App() {
 
   const [wordsJSON, setWordsJSON] = useState("");
-
-  const read_data = localStorage.getItem("words");
-  if(read_data){
-    console.log("READ DATA")
-    console.log(read_data)
-  }else{
-    fetch('https://docs.google.com/spreadsheets/d/1eXIWcvWmPaMkTYBByzaoEuq-NgAuwXTkXDgL_OV937Y/export?gid=0&format=csv')
-    .then((response) => response.text())
-    .then((csv) => {
-      // csv is .csv string
-      //console.log(data)
-      var data = Papa.parse(csv).data;
-      data.shift()
-      //console.log(data)
-
-      let words = [] // long list of 2-element lists, where [0]th is english and [1]th element is polish
-      data.forEach(function (item, index) {
-        let word = [item[0], item[1]]
-        words.push(word)
-      })
-      localStorage.setItem("words", JSON.stringify(words));
-      setWordsJSON(words)
-    });
+  const [randomWord, setRandomWord] = useState();
+  
+  function getData(reset){
+    const read_data = localStorage.getItem("words");
+    if(read_data && reset == false){
+      console.log("READ DATA")
+      setWordsJSON(JSON.parse(read_data))
+    }else{
+      console.log("requesting new data")
+      fetch('https://docs.google.com/spreadsheets/d/1eXIWcvWmPaMkTYBByzaoEuq-NgAuwXTkXDgL_OV937Y/export?gid=0&format=csv')
+      .then((response) => response.text())
+      .then((csv) => {
+        // csv is .csv string
+        var data = Papa.parse(csv).data; // parsing csv to json, which is list of lists representing each row
+        data.shift() // removing header
+  
+        let words = [] // long list of 2-element lists, where [0]th is english and [1]th element is polish
+        data.forEach(function (item, index) {
+          let word = [item[0], item[1]]
+          words.push(word)
+        })
+        localStorage.setItem("words", JSON.stringify(words));
+        setWordsJSON(words)
+      });
+    }
   }
 
+  function pickRandomWord(reset){
+    if(wordsJSON && (!randomWord || reset===true)){
+      const pickedWord = wordsJSON[Math.floor(Math.random()*wordsJSON.length)];
+      console.log(pickedWord)
+      console.log("PICKED RANDOMM WORLDD")
+      setRandomWord(pickedWord)
+    }
+  }
+
+  useEffect(() => {
+    console.log(wordsJSON)
+    if(!wordsJSON){
+      getData(false)
+    }
+  }, []);
+  pickRandomWord()
+
+  let website;
+  if (randomWord){
+    website = (
+      <div>
+        <Card word={randomWord} pickRandomWord={pickRandomWord} />
+        <button className='reset_button' onClick={() => getData(true)}> reset cache </button>
+      </div>
+    )
+  }else{
+    website = <Loading/>
+  }
 
   return (
     <div className="App">
-      <Loading/>
+      {website}   
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
 
 function Card(props) {
+
+  const [side, setSide] = useState(1);
+
+  function switchSide(){
+    if(side === 1){
+        setSide(0)
+    }else{
+        setSide(1)
+    }
+  }
+
   return (
     <div className="card">
-        card
-        {props.words}
+        
+        <h1> {props.word[side]} </h1>
+
+        <div className='button_container'>
+            <div className='div1' onClick={() => props.pickRandomWord(true)}> new word </div>
+            <div className='div2' onClick={() => switchSide()}> flip the card </div>
+        </div>
     </div>
   );
 }
